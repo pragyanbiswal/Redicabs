6f1fa1ecc69413ae3c3e22b0110d91ea54fadc17
pragyanbiswal <pragyanbiswal.intelcorp@gmail.com>
2022-01-27 17:25:04 +0530

    change vehicle

--- admin/post-avehical.php
+++ admin/post-avehical.php
@@ -10,85 +10,21 @@ else{
 
 if(isset($_POST['submit']))
   {
-  $ownname=$_POST['ownname'];
-   $ContactNo=$_POST['ContactNo'];
-    $email=$_POST['email'];
-     $vehno=$_POST['vehno'];
-      $vehreg=$_POST['vehreg'];
-       $vehchas=$_POST['vehchas'];	
-$vehicletitle=$_POST['vehicletitle'];
-$brand=$_POST['brandname'];
-$vehicleoverview=$_POST['vehicalorcview'];
-$priceperday=$_POST['priceperday'];
-$fueltype=$_POST['fueltype'];
-$modelyear=$_POST['modelyear'];
-$seatingcapacity=$_POST['seatingcapacity'];
-$vimage1=$_FILES["img1"]["name"];
-$vimage2=$_FILES["img2"]["name"];
-$vimage3=$_FILES["img3"]["name"];
-$vimage4=$_FILES["img4"]["name"];
-$vimage5=$_FILES["img5"]["name"];
-$airconditioner=$_POST['airconditioner'];
-$powerdoorlocks=$_POST['powerdoorlocks'];
-$antilockbrakingsys=$_POST['antilockbrakingsys'];
-$brakeassist=$_POST['brakeassist'];
-$powersteering=$_POST['powersteering'];
-$driverairbag=$_POST['driverairbag'];
-$passengerairbag=$_POST['passengerairbag'];
-$powerwindow=$_POST['powerwindow'];
-$cdplayer=$_POST['cdplayer'];
-$centrallocking=$_POST['centrallocking'];
-$crashcensor=$_POST['crashcensor'];
-$leatherseats=$_POST['leatherseats'];
-move_uploaded_file($_FILES["img1"]["tmp_name"],"img/vehicleimages/".$_FILES["img1"]["name"]);
-move_uploaded_file($_FILES["img2"]["tmp_name"],"img/vehicleimages/".$_FILES["img2"]["name"]);
-move_uploaded_file($_FILES["img3"]["tmp_name"],"img/vehicleimages/".$_FILES["img3"]["name"]);
-move_uploaded_file($_FILES["img4"]["tmp_name"],"img/vehicleimages/".$_FILES["img4"]["name"]);
-move_uploaded_file($_FILES["img5"]["tmp_name"],"img/vehicleimages/".$_FILES["img5"]["name"]);
-
-$sql="INSERT INTO tblvehicles(ownname,ContactNo,email,vehno,vehreg,vehchas,VehiclesTitle,VehiclesBrand,VehiclesOverview,PricePerDay,FuelType,ModelYear,SeatingCapacity,Vimage1,Vimage2,Vimage3,Vimage4,Vimage5,AirConditioner,PowerDoorLocks,AntiLockBrakingSystem,BrakeAssist,PowerSteering,DriverAirbag,PassengerAirbag,PowerWindows,CDPlayer,CentralLocking,CrashSensor,LeatherSeats) VALUES(:ownname,:ContactNo,:email,:vehno,:vehreg,:vehchas,:vehicletitle,:brand,:vehicleoverview,:priceperday,:fueltype,:modelyear,:seatingcapacity,:vimage1,:vimage2,:vimage3,:vimage4,:vimage5,:airconditioner,:powerdoorlocks,:antilockbrakingsys,:brakeassist,:powersteering,:driverairbag,:passengerairbag,:powerwindow,:cdplayer,:centrallocking,:crashcensor,:leatherseats)";
-$query = $dbh->prepare($sql);
-$query->bindParam(':ownname',$ownname,PDO::PARAM_STR);
-$query->bindParam(':ContactNo',$ContactNo,PDO::PARAM_STR);
-$query->bindParam(':email',$email,PDO::PARAM_STR);
-$query->bindParam(':vehno',$vehno,PDO::PARAM_STR);
-$query->bindParam(':vehreg',$vehreg,PDO::PARAM_STR);
-$query->bindParam(':vehchas',$vehchas,PDO::PARAM_STR);
-$query->bindParam(':vehicletitle',$vehicletitle,PDO::PARAM_STR);
-$query->bindParam(':brand',$brand,PDO::PARAM_STR);
-$query->bindParam(':vehicleoverview',$vehicleoverview,PDO::PARAM_STR);
-$query->bindParam(':priceperday',$priceperday,PDO::PARAM_STR);
-$query->bindParam(':fueltype',$fueltype,PDO::PARAM_STR);
-$query->bindParam(':modelyear',$modelyear,PDO::PARAM_STR);
-$query->bindParam(':seatingcapacity',$seatingcapacity,PDO::PARAM_STR);
-$query->bindParam(':vimage1',$vimage1,PDO::PARAM_STR);
-$query->bindParam(':vimage2',$vimage2,PDO::PARAM_STR);
-$query->bindParam(':vimage3',$vimage3,PDO::PARAM_STR);
-$query->bindParam(':vimage4',$vimage4,PDO::PARAM_STR);
-$query->bindParam(':vimage5',$vimage5,PDO::PARAM_STR);
-$query->bindParam(':airconditioner',$airconditioner,PDO::PARAM_STR);
-$query->bindParam(':powerdoorlocks',$powerdoorlocks,PDO::PARAM_STR);
-$query->bindParam(':antilockbrakingsys',$antilockbrakingsys,PDO::PARAM_STR);
-$query->bindParam(':brakeassist',$brakeassist,PDO::PARAM_STR);
-$query->bindParam(':powersteering',$powersteering,PDO::PARAM_STR);
-$query->bindParam(':driverairbag',$driverairbag,PDO::PARAM_STR);
-$query->bindParam(':passengerairbag',$passengerairbag,PDO::PARAM_STR);
-$query->bindParam(':powerwindow',$powerwindow,PDO::PARAM_STR);
-$query->bindParam(':cdplayer',$cdplayer,PDO::PARAM_STR);
-$query->bindParam(':centrallocking',$centrallocking,PDO::PARAM_STR);
-$query->bindParam(':crashcensor',$crashcensor,PDO::PARAM_STR);
-$query->bindParam(':leatherseats',$leatherseats,PDO::PARAM_STR);
-$query->execute();
-$lastInsertId = $dbh->lastInsertId();
-if($lastInsertId)
-{
-$msg="Vehicle posted successfully";
+ 
+  extract($_POST); 
+  $sql = $sql="INSERT INTO tblvehicles(ownname,ContactNo,email,vehno,vehreg,vehchas,VehiclesTitle,VehiclesBrand,VehiclesOverview,PricePerDay,FuelType,ModelYear,SeatingCapacity,Vimage1,Vimage2,Vimage3,Vimage4,Vimage5,AirConditioner,PowerDoorLocks,AntiLockBrakingSystem,BrakeAssist,PowerSteering,DriverAirbag,PassengerAirbag,PowerWindows,CDPlayer,CentralLocking,CrashSensor,LeatherSeats) VALUES($ownname,$ContactNo,$email,$vehno,$vehreg,$vehchas,$vehicletitle,$brand,$vehicleoverview,$priceperday,$fueltype,$modelyear,$seatingcapacity,$vimage1,$vimage2,$vimage3,$vimage4,$vimage5,$airconditioner,$powerdoorlocks,$antilockbrakingsys,$brakeassist,$powersteering,$driverairbag,$passengerairbag,$powerwindow,$cdplayer,$centrallocking,$crashcensor,$leatherseats)";
+  $res=mysqli_query($conn, $sql);
+  if($res){
+  $msg="<b class='succWrap'>Vehicle posted Successfully</b>";
+  } else {
+  	$msg="<b class='errorWrap'>Vehicle  Failed</b>";
+  }    
 }
-else 
-{
-$error="Something went wrong. Please try again";
-}
-
+// move_uploaded_file($_FILES["img1"]["tmp_name"],"img/vehicleimages/".$_FILES["img1"]["name"]);
+// move_uploaded_file($_FILES["img2"]["tmp_name"],"img/vehicleimages/".$_FILES["img2"]["name"]);
+// move_uploaded_file($_FILES["img3"]["tmp_name"],"img/vehicleimages/".$_FILES["img3"]["name"]);
+// move_uploaded_file($_FILES["img4"]["tmp_name"],"img/vehicleimages/".$_FILES["img4"]["name"]);
+// move_uploaded_file($_FILES["img5"]["tmp_name"],"img/vehicleimages/".$_FILES["img5"]["name"]);
 }
 
 
@@ -159,10 +95,10 @@ $error="Something went wrong. Please try again";
 							<div class="col-md-12">
 								<div class="panel panel-default">
 									<div class="panel-heading">Basic Info</div>
-<?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
-				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
+  <?php echo $msg."<br><br>"; ?>
 
 									<div class="panel-body">
+
 <form method="post" class="form-horizontal" enctype="multipart/form-data">
 	<div class="form-group">
 <label class="col-sm-2 control-label">OwnerName<span style="color:red">*</span></label>
@@ -203,7 +139,8 @@ $error="Something went wrong. Please try again";
 <div class="col-sm-4">
 <select class="selectpicker" name="brandname" required>
 <option value=""> Select </option>
-<?php $ret="select id,BrandName from tblbrands";
+<?php 
+$ret="select id,BrandName from tblbrands";
 $query= $dbh -> prepare($ret);
 //$query->bindParam(':id',$id, PDO::PARAM_STR);
 $query-> execute();
